f0a100ef3d27993e1bee7b1d59400bd3f975b6e4
Acarb046 <acarb046@fiu.edu>
2024-02-17 01:42:20 -0500

    Connected Database, ran command to inspect connected database but not now to do cleanup.

--- CEN4010_GeekText/settings.py
+++ CEN4010_GeekText/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Feature_Wishlist.apps.FeatureWishlistConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'CEN4010_GeekText.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'GeekText',
+        'USER': 'postgres',
+        'PASSWORD': 'Beerg33k!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
