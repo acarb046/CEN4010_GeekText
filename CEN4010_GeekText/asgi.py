dd25fed1f0b256586f60d97906bf3e4b500c1b42
Acarb046 <acarb046@fiu.edu>
2024-02-17 00:22:27 -0500

    New Project Repository

---
+++ CEN4010_GeekText/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CEN4010_GeekText project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CEN4010_GeekText.settings')
+
+application = get_asgi_application()
