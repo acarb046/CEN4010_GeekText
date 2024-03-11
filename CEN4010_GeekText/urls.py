de318990b547c38b101c3a10299cc64ff5179ab2
sanayahbordes <106409119+bsanayah@users.noreply.github.com>
2024-03-10 23:27:10 -0400

    Add files via upload

--- CEN4010_GeekText/urls.py
+++ CEN4010_GeekText/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from NewBookRC import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('NewBookRC.urls')),
+    #path('api/', include('rest_framework.urls')),
+    #path('ratings-comments/', include('NewBookRC.urls')),  # Include other app URLs
+    path('', include('NewBookRC.urls')),  # You may replace 'NewBookRC.urls' with the appropriate URL pattern
+
 ]
