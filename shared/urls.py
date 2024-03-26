5a9b256608130aaa986cb43fe94de613afee1b16
acarr263 <158006155+acarr263@users.noreply.github.com>
2024-03-26 13:26:26 -0400

    Added models.

--- shared/urls.py
+++ shared/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
-
 router = DefaultRouter()
+
+##
+##
+
+from .views import UserViewSet
 router.register(r'users', UserViewSet)
+
+from .views import BookViewSet
+router.register(r'books', BookViewSet)
+
+from .views import ShoppingCartViewSet
+router.register(r'shopping-carts', ShoppingCartViewSet)
+
+from .views import ShoppingCartItemViewSet
+router.register(r'shopping-cart-items', ShoppingCartItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- shared/views.py
+++ shared/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
+##
+##
+
 from .models import User
 from .serializers import UserSerializer
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+from .models import Book
+from .serializers import BookSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+from .models import ShoppingCart
+from .serializers import ShoppingCartSerializer
+class ShoppingCartViewSet(viewsets.ModelViewSet):
+    queryset = ShoppingCart.objects.all()
+    serializer_class = ShoppingCartSerializer
+
+from .models import ShoppingCartItem
+from .serializers import ShoppingCartItemSerializer
+class ShoppingCartItemViewSet(viewsets.ModelViewSet):
+    queryset = ShoppingCartItem.objects.all()
+    serializer_class = ShoppingCartItemSerializer
